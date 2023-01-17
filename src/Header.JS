b5cf0de66f7ff2b7bee5366b7e0280c0ada2c646
febiasm <trevorfebias@gmail.com>
2023-01-17 11:50:44 +0300

    implimented css

---
+++ src/Header.JS
@@ -0,0 +1,15 @@
+import React from "react"
+function Header (){
+return(
+<thead>
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+)
+}
+export default Header
